71a6ba481fc93125287ed17d5a031f30ea5df3e9
ChoiEunCheol <wndqhrgkdlel@gmail.com>
2023-10-29 19:26:29 +0900

    문제7 완료

---
+++ Q7.js
@@ -0,0 +1,8 @@
+// JavaScript에서 null과 undefined의 차이점을 설명하세요.
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(undefined==null);
+console.log(undefined===null);
+
+// null 은 값이 비어있는 것 (의도적으로 비워두는 느낌)
+// undefined 는 값이 정의되지 않은 것
